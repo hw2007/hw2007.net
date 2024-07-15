f6afd138aa48d5cbbc62fe5d4e09d13339a5ed9f
hw2007 <hw2007official@gmail.com>
2024-07-14 21:26:41 -0500

    Added a basic speedtest page using OpenSpeedTest

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-rsync -avrP --delete --exclude=sync.sh ~/Desktop/Coding/hw2007.net/ braden@halsey:/mnt/data/website/
+rsync -avrP --delete --exclude=sync.sh --exclude=".git/" --exclude=".gitignore" ~/Desktop/Coding/hw2007.net/ braden@halsey:/mnt/data/website/
