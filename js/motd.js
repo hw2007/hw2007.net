8c16136b9fe0d9b2f5c1e4b4a40a430b3174d180
hw2007 <hw2007official@gmail.com>
2024-07-16 22:16:33 -0500

    Add OS-aware motd

--- js/motd.js
+++ js/motd.js
@@ -1,3 +1,26 @@
+// Used for one specific MOTD which displays the user's OS
+function getOS() {
+    let userAgent = navigator.userAgent;
+    let platform = navigator.platform;
+    let os = "computer";
+
+    if (platform.startsWith("Win")) {
+        os = "windows";
+    } else if (platform.startsWith("Mac")) {
+        os = "macOS";
+    } else if (platform.startsWith("Linux")) {
+        os = "linux";
+    } else if (/Android/.test(userAgent)) {
+        os = "android";
+    } else if (/iPhone|iPad|iPod/.test(userAgent)) {
+        os = "iOS";
+    }
+
+    return os;
+}
+
+console.log("The user's operating system is: " + getOS());
+
 // List of MOTDs to display on load
 const messages = [
 	"the finest corner of the internet.",
@@ -12,7 +35,8 @@ const messages = [
 	"brought to you by hw2007 and chatgpt!",
 	"engage!",
 	"welcome, traveller!",
-	"there are four lights!"
+	"there are four lights!",
+	`hello, ${getOS()} user! :)`
 ];
 
 // Function to display a random message
