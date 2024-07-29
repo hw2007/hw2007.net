758e873956615e7dc4a8a1cea9e367d29067c76b
hw2007 <hw2007official@gmail.com>
2024-07-29 12:32:43 -0500

    -Stylistic changes to expand/shrink buttons on map page -Add animation when toggling expanded map view -Update changelogs -Fix dates being June rather than July in changelogs page

--- js/map-expand.js
+++ js/map-expand.js
@@ -1,11 +1,13 @@
 function expand() {
     document.getElementById("map").style.top = "0px";
-    document.getElementById("shrink-container").style.display = "inline-block";
+    document.getElementById("expand").style.display = "none";
+    document.getElementById("shrink").style.display = "inline-block";
 }
 
 function shrink() {
     document.getElementById("map").style.top = "60px";
-    document.getElementById("shrink-container").style.display = "none";
+    document.getElementById("expand").style.display = "inline-block";
+    document.getElementById("shrink").style.display = "none";
 }
 
 document.getElementById("expand").addEventListener("click", expand);
