145fc25bf15a2b8c414321b2a1a15f588810514f
hw2007 <hw2007official@gmail.com>
2024-08-24 00:12:07 -0500

    -> Small changes to game page paddings -> Map page now enters proper fullscreen on pushing the fullscreen button

--- js/map-expand.js
+++ js/map-expand.js
@@ -1,11 +1,28 @@
+function enterFullscreen() {
+    const iframe = document.getElementById("map");
+
+    if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+    } else if (iframe.mozRequestFullScreen) { // Firefox
+        iframe.mozRequestFullScreen();
+    } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+        iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) { // IE/Edge
+        iframe.msRequestFullscreen();
+    }
+    else {
+        document.getElementById("container").style.top = "0px";
+        document.getElementById("expand").style.display = "none";
+        document.getElementById("shrink").style.display = "inline-block";
+    }
+}
+
 function expand() {
-    document.getElementById("map").style.top = "0px";
-    document.getElementById("expand").style.display = "none";
-    document.getElementById("shrink").style.display = "inline-block";
+    enterFullscreen();
 }
 
 function shrink() {
-    document.getElementById("map").style.top = "60px";
+    document.getElementById("container").style.top = "60px";
     document.getElementById("expand").style.display = "inline-block";
     document.getElementById("shrink").style.display = "none";
 }
