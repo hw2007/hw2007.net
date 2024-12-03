c6e2163c98dbe506790e742e5be414d8fdffbadf
hw2007 <braden@jnjschneider.com>
2024-12-02 20:57:45 -0600

    Update market miner link

--- js/game-grid-generator.js
+++ js/game-grid-generator.js
@@ -5,7 +5,7 @@ const games = [
 		img: "marketminer",
 		url:
 			"itchembed.html?url=" +
-			encodeURIComponent("https://itch.io/embed-upload/12144162?color=000000"),
+			encodeURIComponent("https://itch.io/embed-upload/12152911?color=000000"),
 		controls: "Click/Enter, P",
 	},
 	{
