54621c7e1e358ab00591b4d0958097ca58567bb0
hw2007 <braden@jnjschneider.com>
2024-12-25 14:01:26 -0600

    misc

--- php/get_downloads.php
+++ php/get_downloads.php
@@ -1,12 +1,12 @@
 <?php
-	// Path to the downloads folder
-	$downloads =  "../downloads";
-	
-	// Scan for files in the downloads folder
-	$files = glob($downloads . "/*.zip");
-	$files = array_map("basename", $files);
-	
-	// Return the files as JSON (readable by javascript)
-	header("Content-Type: application/json");
-	echo json_encode($files);
+// Path to the downloads folder
+$downloads = "../downloads";
+
+// Scan for files in the downloads folder
+$files = glob($downloads . "/*.zip");
+$files = array_map("basename", $files);
+
+// Return the files as JSON (readable by javascript)
+header("Content-Type: application/json");
+echo json_encode($files);
 ?>
